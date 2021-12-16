6afe44d44769f5fedfaeb4bef9c11b2bb507b72f
ZHChain <chain_13918925958@outlook.com>
2021-12-16 16:33:55 +0800

    Finished Home app

--- TranslHero/urls.py
+++ TranslHero/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import Home.views as home_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # Home
+    path('', home_views.home),
 ]
